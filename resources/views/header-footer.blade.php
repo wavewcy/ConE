e7c95354250554853cc6da2df89f3b9371763933
thipaporn <thipaporn5099@gmail.com>
2021-03-17 13:31:29 +0700

    youtube

--- resources/views/header-footer.blade.php
+++ resources/views/header-footer.blade.php
@@ -522,7 +522,7 @@
 			<div class="wrap-video-mo-01">
 				<div class="w-full wrap-pic-w op-0-0"><img src="images/icons/video-16-9.jpg" alt="IMG"></div>
 				<div class="video-mo-01">
-					<iframe src="https://video-amt2-1.xx.fbcdn.net/v/t42.1790-2/47063193_280704982790236_3225585825878114304_n.mp4?_nc_cat=101&vs=fc9f4c1c08bc1ec3&_nc_vs=HBksFQAYJEdKa2d6Z0pjTU1tNFRQOEFBQUFBQUFBNGxjTXNidjRHQUFBRhUAAsgBABUAGCRHSVd1emdLRXJrQlI2d0VCQUFBQUFBQzBGOFZoYnY0R0FBQUYVAgLIAQBLAogScHJvZ3Jlc3NpdmVfcmVjaXBlATEgbWVhc3VyZV9vcmlnaW5hbF9yZXNvbHV0aW9uX3NzaW0AFQAlABwAACb6yLW307z5BhUCKAJDMxgLdnRzX3ByZXZpZXccF0BDdLxqfvnbGBhkYXNoX3Y0X2hxMl9mcmFnXzJfdmlkZW8SABgYdmlkZW9zLnZ0cy5jYWxsYmFjay5wcm9kOBJWSURFT19WSUVXX1JFUVVFU1QbCIgVb2VtX3RhcmdldF9lbmNvZGVfdGFnBm9lcF9oZBNvZW1fcmVxdWVzdF90aW1lX21zDTE2MTQxNDI2NDA5ODgMb2VtX2NmZ19ydWxlB3VubXV0ZWQTb2VtX3JvaV9yZWFjaF9jb3VudAQxMzM0DG9lbV92aWRlb19pZBAxOTU1OTczNjc0NTE4NzE2Em9lbV92aWRlb19hc3NldF9pZBAxOTU1OTczNjY3ODUyMDUwFW9lbV92aWRlb19yZXNvdXJjZV9pZBAxOTU1OTczNjY0NTE4NzE3HG9lbV9zb3VyY2VfdmlkZW9fZW5jb2RpbmdfaWQPMjgwNzA0OTc5NDU2OTAzJQIcAAA%3D&ccb=3&_nc_sid=a6057a&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCJ9&_nc_ohc=O0Bjo7Oqy3cAX--rKsE&_nc_ht=video-amt2-1.xx&oh=0acb03643b4b6ad9ce54b537ebb41fd5&oe=6035FD1B&_nc_rid=dabc8e409f0c4e3" allowfullscreen></iframe>
+					<iframe src="https://www.youtube.com/watch?v=ZBAcrzsHXUw" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
